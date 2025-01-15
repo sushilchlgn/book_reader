c72656e62b2403f1988d3fede9da105fb5a23fd1
sushil chaulagain <sushilchlgn@gmail.com>
2025-01-15 15:55:58 +0545

    added import screen

--- navigation/AppNavigator.js
+++ navigation/AppNavigator.js
@@ -2,15 +2,17 @@ import React from 'react';
 import { NavigationContainer } from '@react-navigation/native';
 import { createStackNavigator } from '@react-navigation/stack';
 import HomeScreen from '../screens/HomeScreen';
-// import BookViewerScreen from '../screens/BookViewerScreen';
+import BookViewerScreen from '../screens/BookViewerScreen';
+import ImportScreen from '../screens/ImportScreen';
 
 const Stack = createStackNavigator();
 
 const AppNavigator = () => (
   <NavigationContainer>
-    <Stack.Navigator>
+    <Stack.Navigator initialRouteName="Home">
       <Stack.Screen name="Home" component={HomeScreen} />
-      {/* <Stack.Screen name="BookViewer" component={BookViewerScreen} /> */}
+      <Stack.Screen name="BookViewer" component={BookViewerScreen} />
+      <Stack.Screen name="Import" component={ImportScreen} />
     </Stack.Navigator>
   </NavigationContainer>
 );
