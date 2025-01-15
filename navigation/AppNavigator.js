dafda844f21f31d36c920aa92c6b27072375371a
sushil chaulagain <sushilchlgn@gmail.com>
2025-01-15 16:11:03 +0545

    homepage

--- navigation/AppNavigator.js
+++ navigation/AppNavigator.js
@@ -1,8 +1,8 @@
 import React from 'react';
 import { NavigationContainer } from '@react-navigation/native';
 import { createStackNavigator } from '@react-navigation/stack';
-// import HomeScreen from '../screens/HomeScreen';
-import BookViewerScreen from '../screens/BookViewerScreen';
+import HomeScreen from '../screens/HomeScreen';
+// import BookViewerScreen from '../screens/BookViewerScreen';
 // import ImportScreen from '../screens/ImportScreen';
 
 const Stack = createStackNavigator();
@@ -10,7 +10,7 @@ const Stack = createStackNavigator();
 const AppNavigator = () => (
   <NavigationContainer>
     <Stack.Navigator initialRouteName="Home">
-      <Stack.Screen name="Home" component={BookViewerScreen} />
+      <Stack.Screen name="Home" component={HomeScreen} />
     </Stack.Navigator>
   </NavigationContainer>
 );
